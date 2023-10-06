2f2db623135cd99b4761894f9f6faef5f329783b
Milan Artner <xmilken01x@gmail.com>
2023-10-06 11:19:03 +0200

    added clock

--- index.js
+++ index.js
@@ -8,13 +8,18 @@ function isAddress(input) {
   if (input.includes(' ')) return false;
 
   try {
-    const url = new URL(input);
-    return true; // Parses as a URL, likely an address.
+    return new URL(input);
   } catch (error) {
-    // URL parsing failed, continue with other checks.
+    try {
+      return input.includes('.') ? new URL(`http://${input}`) : false;
+    } catch (error) {}
   }
 
   return false;
+}
+
+function makeAddress(input) {
+  return input.includes('http://') || input.includes('https://') ? input : `http://${input}`;
 }
 
 function makeSearch(input) {
@@ -35,9 +40,7 @@ function redirectTo(url, target = '_self') {
 function parseUrlParams(url) {
   const params = {};
   const paramStr = url.split('?')[1];
-
   if (!paramStr) return params;
-
   paramStr.split('&').forEach((pair) => {
     const [key, value] = pair.split('=');
     if (key && value) params[key] = decodeURIComponent(value);
@@ -45,14 +48,33 @@ function parseUrlParams(url) {
   return params;
 }
 
+function updateTime() {
+  const now = new Date();
+  const formattedTime = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false });
+  const formattedDate = now.toLocaleDateString([], { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+
+  document.getElementById('clock').textContent = formattedTime;
+  document.getElementById('date').textContent = formattedDate;
+
+  setInterval(updateTime, 1000);
+}
+
 function init() {
-  const link = parseUrlParams(window.location.href).link;
-  if (link) redirectTo(links[link]);
+  const redirect = parseUrlParams(window.location.href).r;
+  if (redirect) redirectTo(links[redirect]);
   searchBar.value = '';
   searchBar.addEventListener('keydown', function (event) {
     const query = searchBar.value;
-    if (event.key === 'Enter') redirectTo(isAddress(query) ? query : makeSearch(query));
+    if (event.key === 'Enter') redirectTo(isAddress(query) ? makeAddress(query) : makeSearch(query));
   });
+  updateTime();
 }
 
 window.addEventListener('load', init);
+
+const circle = document.querySelector('#circle');
+
+/*window.addEventListener('mousemove', (e) => {
+  circle.style.left = e.clientX + 'px';
+  circle.style.top = e.clientY + 'px';
+});*/
